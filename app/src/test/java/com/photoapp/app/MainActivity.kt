146225987984148637d6200f2ed51172de434ac9
CsNinja34 <fa30576@seeu.edu.mk>
2024-02-04 17:13:56 +0000

    Code Synced by DhiWise

---
+++ app/src/test/java/com/photoapp/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.photoapp.app
+
+import com.photoapp.app.appcomponents.base.BaseActivity
+import com.photoapp.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
